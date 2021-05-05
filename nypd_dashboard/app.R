6480ecbc51680446af7df0461ae97dfa3292667a
mariumtapal <mariumtapal@gmail.com>
2021-05-05 17:48:08 -0400

    create scratchpads and sources for components

---
+++ nypd_dashboard/app.R
@@ -0,0 +1,64 @@
+# load in required packages
+library(shiny)
+library(shinythemes)
+library(leaflet)
+library(reactable)
+library(readxl)
+library(janitor)
+library(tidyverse)
+library(readtext)
+library(reactable)
+library(plotly)
+
+# load in data
+allegations <- read_csv("data/allegations.csv") 
+allegations_2019 <- allegations %>%  filter(year_received == 2019)
+
+# load in source files
+source("home.R")
+source("allegations.R")
+source("precincts.R")
+source("demographics.R")
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application theme
+    theme = shinytheme("paper"),
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
