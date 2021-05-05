97cff03201f7a71d967cac31bf18cc62bc2092af
mariumtapal <mariumtapal@gmail.com>
2021-05-05 19:52:16 -0400

    tabs + footer

--- dashboard/app.R
+++ dashboard/app.R
@@ -26,39 +26,30 @@ source("demographics.R")
 ui <- fluidPage(
     
     # Application theme
-    theme = shinytheme("paper"),
+    theme = shinytheme("flatly"),
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("NYC Civilian Complaints About PD"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    # Create tabs
+    tabsetPanel(
+        tabPanel("Home"),
+        tabPanel("Allegations"),
+        tabPanel("NYC Precincts"),
+        tabPanel("Race/Gender Demographies")
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    
+    # Footer
+    div(
+        class = "footer",
+        includeHTML("footer.html")
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    # put in output components here
 }
 
 # Run the application 
