40fcd9ef33f64c3b985f42b0bf32b1a4fce71472
mariumtapal <mariumtapal@gmail.com>
2021-05-10 22:37:38 -0400

    mariums work

--- dashboard/app.R
+++ dashboard/app.R
@@ -12,8 +12,11 @@ library(plotly)
 library(here)
 
 # load in data
-allegations <- read_csv(here("data", "allegations.csv")) 
+allegations <- read_csv(here("data", "allegations.csv"))
+# 2019 till demographics file is updated
 allegations_2019 <- allegations %>% filter(year_received == 2019)
+allegations_2016 <- allegations %>% filter(year_received == 2016)
+allegations_2018 <- allegations %>% filter(year_received == 2018)
 
 # load in source files
 source("home.R")
@@ -24,33 +27,51 @@ source("demographics.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
-    # Application theme
-    theme = shinytheme("flatly"),
 
-    # Application title
-    titlePanel("NYC Civilian Complaints About PD"),
+  # Application theme
+  theme = shinytheme("flatly"),
 
-    # Create tabs
-    tabsetPanel(
-        tabPanel("Home"),
-        tabPanel("Allegations"),
-        tabPanel("NYC Precincts"),
-        tabPanel("Race/Gender Demographics")
-        ),
-    
-    # Footer
-    div(
-        class = "footer",
-        includeHTML("footer.html")
-    )
+  # Application title
+  titlePanel("NYC Civilian Complaints About PD"),
+
+  # Create tabs
+  tabsetPanel(
+    tabPanel("Home"),
+    tabPanel("Allegations"),
+    tabPanel("NYC Precincts",
+             p("The following interactive map shows the number of allegations 
+             against police officers in each NYC precinct in the years 2016 and 2018. 
+             You can toggle each layer and click on the marker to this information."),
+             leafletOutput("leaflet_year"),
+             p(),
+             p("This interactive map shows the number of allegations 
+             against police officers in each NYC precinct in the years 2016 and 2018 
+             (combined) by borough. You can toggle each layer and click on the marker 
+             to this information."),
+             leafletOutput("leaflet_borough"),
+             p()),
+    tabPanel("Race/Gender Demographics")
+  ),
+
+  # Footer
+  div(
+    class = "footer",
+    includeHTML("footer.html")
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    # put in output components here
+  # put in output components here
+  output$leaflet_year <- renderLeaflet({
+    leaflet_year
+  })
+
+  output$leaflet_borough <- renderLeaflet({
+    leaflet_borough
+  })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
