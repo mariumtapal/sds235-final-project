6b87366fa48321d92e7717c8303a4126ab5b41fa
mariumtapal <mariumtapal@gmail.com>
2021-05-14 14:47:59 -0400

    precinct mapssss

--- dashboard/app.R
+++ dashboard/app.R
@@ -14,7 +14,6 @@ library(here)
 # load in data
 allegations <- read_csv(here("data", "allegations.csv"))
 # 2019 till demographics file is updated
-allegations_2019 <- allegations %>% filter(year_received == 2019)
 allegations_2016 <- allegations %>% filter(year_received == 2016)
 allegations_2018 <- allegations %>% filter(year_received == 2018)
 
@@ -27,99 +26,140 @@ source("demographics.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
-    # Application theme
-    theme = shinytheme("flatly"),
 
-    # Application title
-    titlePanel("NYC Civilian Complaints About PD"),
+  # Application theme
+  theme = shinytheme("flatly"),
 
-    # Create tabs
-    tabsetPanel(
-        tabPanel("Home"),
-        tabPanel("Allegations"),
-        tabPanel("NYC Precincts",
-                 p("The following interactive map shows the number of allegations 
-             against police officers in each NYC precinct in the years 2016 and 2018. 
-             You can toggle each layer and click on the marker to this information."),
-                 leafletOutput("leaflet_year"),
-                 p(),
-                 p("This interactive map shows the number of allegations 
+  # Application title
+  titlePanel("NYC Civilian Complaints About PD"),
+
+  # Create tabs
+  tabsetPanel(
+    tabPanel("Home"),
+    tabPanel("Allegations"),
+    tabPanel(
+      "NYC Precincts",
+      p("This interactive map shows the number of allegations 
              against police officers in each NYC precinct in the years 2016 and 2018 
-             (combined) by borough. You can toggle each layer and click on the marker 
-             to this information."),
-                 leafletOutput("leaflet_borough"),
-                 p()),
-        tabPanel("Race/Gender Demographics",
-                 fluidRow(
-                   column(3, "Officer Demographics",
-                          selectInput("select_year1",
-                                      label = "Year",
-                                      choices = list(2016,
-                                                     2018)
-                          ),
-                          selectInput("select_variable",
-                                      label = "Demographic",
-                                      choices = list("Race",
-                                                     "Gender"))),
-                   column(9,
-                          textOutput("output_year1"),
-                          textOutput("output_variable"),
-                          plotlyOutput("mos_race"))
-                 ),
-                 p(),
-                 fluidRow(
-                   column(3, "Race of Officers and Complainants",
-                          selectInput("select_year",
-                                      label = "Year",
-                                      choices = list(2016,
-                                                     2018)
-                          )),
-                   column(9,
-                          textOutput("output_year"),
-                          plotlyOutput("race_plot"))
-                 )
-                 )),
-        
-    # Footer
-    div(
-        class = "footer",
-        includeHTML("footer.html")
+             (combined) by borough. You can toggle for each borough and click on the marker 
+             to view this information. The size of the marks indicate the number of allegations."),
+      fluidRow(
+        column(
+          3,
+          selectInput("select_year_leaflet",
+            label = "Precinct Allegations by Borough",
+            choices = list(
+              "2016 and 2018 Combined", 2016,
+              2018
+            )
+          ),
+        ),
+        column(
+          9,
+          textOutput("select_year_leaflet"),
+          leafletOutput("leaflets")
+        )
+      ),
+      p()
+    ),
+    tabPanel(
+      "Race/Gender Demographics",
+      fluidRow(
+        column(
+          3, "Officer Demographics",
+          selectInput("select_year1",
+            label = "Year",
+            choices = list(
+              2016,
+              2018
+            )
+          ),
+          selectInput("select_variable",
+            label = "Demographic",
+            choices = list(
+              "Race",
+              "Gender"
+            )
+          )
+        ),
+        column(
+          9,
+          textOutput("output_year1"),
+          textOutput("output_variable"),
+          plotlyOutput("mos_race")
+        )
+      ),
+      p(),
+      fluidRow(
+        column(
+          3, "Race of Officers and Complainants",
+          selectInput("select_year",
+            label = "Year",
+            choices = list(
+              2016,
+              2018
+            )
+          )
+        ),
+        column(
+          9,
+          textOutput("output_year"),
+          plotlyOutput("race_plot")
+        )
+      )
     )
+  ),
+
+  # Footer
+  div(
+    class = "footer",
+    includeHTML("footer.html")
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    # put in output components here
+  # put in output components here
   output$mos_race <- renderPlotly({
     off <- mos_race
-    if (input$select_year1 == 2016 & input$select_variable == 'Race'){off <- mos_race_2016}
-    else if (input$select_year1 == 2016 & input$select_variable == 'Gender'){off <- mos_gender_2016}
-    else if (input$select_year1 == 2018 & input$select_variable == 'Race'){off <- mos_race_2018}
-    else if (input$select_year1 == 2018 & input$select_variable == 'Gender'){off <- mos_gender_2018}
+    if (input$select_year1 == 2016 & input$select_variable == "Race") {
+      off <- mos_race_2016
+    }
+    else if (input$select_year1 == 2016 & input$select_variable == "Gender") {
+      off <- mos_gender_2016
+    }
+    else if (input$select_year1 == 2018 & input$select_variable == "Race") {
+      off <- mos_race_2018
+    }
+    else if (input$select_year1 == 2018 & input$select_variable == "Gender") {
+      off <- mos_gender_2018
+    }
     off
-    
   })
-   
+
   output$race_plot <- renderPlotly({
     res <- race_plot
-    if (input$select_year == 2016){res <- race_plot_2016}
-    else if (input$select_year == 2018){res <- race_plot_2018}
+    if (input$select_year == 2016) {
+      res <- race_plot_2016
+    }
+    else if (input$select_year == 2018) {
+      res <- race_plot_2018
+    }
     res
-
   })
 
-  output$leaflet_year <- renderLeaflet({
-    leaflet_year
+  output$leaflets <- renderLeaflet({
+    off <- leaflet_all
+    if (input$select_year_leaflet == 2016) {
+      off <- leaflet_2016
+    }
+    else if (input$select_year_leaflet == 2018) {
+      off <- leaflet_2018
+    }
+    off
   })
-
-  output$leaflet_borough <- renderLeaflet({
-    leaflet_borough
-  })
-
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
--- dashboard/precincts.R
+++ dashboard/precincts.R
@@ -4,7 +4,6 @@
 # 3. https://www1.nyc.gov/site/nypd/bureaus/patrol/precincts-landing.page
 
 coordinates <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ8xw99f6gzPHNvINFt5THA4NrungmyCxGhKAUiRF57q7o1anelmhcgFu-1Dkm3wwqYv3kLi4hSF7Wl/pub?output=csv")
-
 allegations <- read_csv(here("data", "allegations.csv"))
 
 # subset allegations for officers at command in precincts and detective squads
@@ -12,58 +11,24 @@ allegations_pct_det <- allegations %>%
   filter(stringr::str_ends(command_now, "PCT") == TRUE | stringr::str_ends(command_now, "DET") == TRUE) %>%
   filter(year_received %in% c(2016, 2018))
 
+# 2016 and 2018 combined -------------------
 # trim whitespaces
 allegations_pct_det$command_now <- str_trim(allegations_pct_det$command_now, side = "both")
 coordinates$command_now <- str_trim(coordinates$command_now, side = "both")
-
 # join coordinate data with allegation data
 map_data <- dplyr::inner_join(allegations_pct_det, coordinates, by = "command_now") %>% unique()
-
 # convert command_now into factor
 map_data <- map_data %>% mutate(command_now = as.factor(command_now))
-
 # create counts for size of circle
 radii <- map_data %>% count(command_now)
 map_data <- map_data %>% left_join(radii, by = "command_now")
-
 # colour palette for boroughs
 boroughCol <- colorFactor(palette = "Paired", map_data$borough)
-
 # pop up label
 popup <- paste0(map_data$precinct.y, ", ", map_data$address, ", ", map_data$borough, "<br>", "Number of Allegations: ", map_data$n)
 
-# yearly layers
-map_data_2016 <- map_data %>% filter(year_received == 2016)
-map_data_2018 <- map_data %>% filter(year_received == 2018)
-
-# base leaflet
-leaflet_year <- map_data %>%
-  leaflet() %>%
-  addTiles() %>%
-  addTiles(group = "Open Street Maps") %>%
-  addCircleMarkers(
-    data = map_data_2016,
-    radius = ~ map_data$n / 10,
-    color = "pink",
-    group = "2016",
-    fillOpacity = 0.2,
-    popup = popup
-  ) %>%
-  addCircleMarkers(
-    data = map_data_2018,
-    radius = ~ map_data$n / 10,
-    color = "lavender",
-    group = "2018",
-    opacity = 0.2,
-    popup = popup
-  ) %>%
-  addLayersControl(
-    overlayGroups = map_data$year_received,
-    options = layersControlOptions(collapsed = FALSE)
-  )
-
-# borough leaflet
-leaflet_borough <- map_data %>%
+# leaflet
+leaflet_all <- map_data %>%
   leaflet() %>%
   addTiles() %>%
   addTiles(group = "Open Street Maps") %>%
@@ -80,13 +45,58 @@ leaflet_borough <- map_data %>%
     options = layersControlOptions(collapsed = FALSE)
   )
 
-# # modules
-# leafletUI <- function(id) {
-#   ns <- NS(id)
-#   tagList(selectInput(ns("year"), "Select Year", c(2016, 2018)))
-#   leafletOutput(ns("leaflet"))
-# }
-#
-# leafletModule <- function(input, output, session) {
-#   output$leaflet <- renderLeaflet({"leaflet"})
-# }
+# 2016 -------------------
+map_data_2016 <- map_data %>% filter(year_received == 2016)
+# create counts for size of circle
+radii_2016 <- map_data_2016 %>% count(command_now)
+map_data_2016 <- map_data_2016 %>% left_join(radii_2016, by = "command_now")
+# colour palette for boroughs
+boroughCol <- colorFactor(palette = "Paired", map_data_2016$borough)
+# pop up label
+popup_2016 <- paste0(map_data_2016$precinct.y, ", ", map_data_2016$address, ", ", map_data_2016$borough, "<br>", "Number of Allegations: ", map_data_2016$n.y)
+
+# leaflet
+leaflet_2016 <- map_data_2016 %>%
+  leaflet() %>%
+  addTiles() %>%
+  addTiles(group = "Open Street Maps") %>%
+  addCircleMarkers(
+    data = map_data_2016,
+    radius = ~ map_data_2016$n.y / 10,
+    color = boroughCol(map_data_2016$borough),
+    group = map_data_2016$borough,
+    fillOpacity = 0.8,
+    popup = popup_2016
+  ) %>%
+  addLayersControl(
+    overlayGroups = map_data_2016$borough,
+    options = layersControlOptions(collapsed = FALSE)
+  )
+
+# 2018 -------------------
+map_data_2018 <- map_data %>% filter(year_received == 2018)
+# create counts for size of circle
+radii_2018 <- map_data_2018 %>% count(command_now)
+map_data_2018 <- map_data_2018 %>% left_join(radii_2018, by = "command_now")
+# colour palette for boroughs
+boroughCol <- colorFactor(palette = "Paired", map_data_2018$borough)
+# pop up label
+popup_2018 <- paste0(map_data_2018$precinct.y, ", ", map_data_2018$address, ", ", map_data_2018$borough, "<br>", "Number of Allegations: ", map_data_2018$n.y)
+
+# leaflet
+leaflet_2018 <- map_data_2018 %>%
+  leaflet() %>%
+  addTiles() %>%
+  addTiles(group = "Open Street Maps") %>%
+  addCircleMarkers(
+    data = map_data_2018,
+    radius = ~ map_data_2018$n.y / 10,
+    color = boroughCol(map_data_2018$borough),
+    group = map_data_2018$borough,
+    fillOpacity = 0.8,
+    popup = popup_2018
+  ) %>%
+  addLayersControl(
+    overlayGroups = map_data_2018$borough,
+    options = layersControlOptions(collapsed = FALSE)
+  )
