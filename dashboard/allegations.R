fed64218b76cfd673aa35107dd41830f4394ffd9
epartakki <epartakki@smith.edu>
2021-05-17 13:47:39 -0400

    Marium please help me

---
+++ dashboard/allegations.R
@@ -0,0 +1,148 @@
+allegations <- read_csv(here("data", "allegations.csv")) 
+allegations_2019 <- allegations %>%  filter(year_received == 2019)
+allegations_2016 <- allegations %>%  filter(year_received == 2016)
+allegations_2018 <- allegations %>%  filter(year_received == 2018)
+
+
+
+#barplot of rank of officers 2016
+mos_officers_2016 <- allegations_2016 %>%
+  mutate(name = paste(first_name, last_name)) %>% 
+  distinct(name, .keep_all = TRUE) %>% 
+  group_by(rank_incident) %>% 
+  count() %>% 
+  mutate(percent = paste(round(n/744*100, digits = 2),'%', sep = "")) %>% 
+  plot_ly(x = ~rank_incident,
+          y = ~n,
+          type = 'bar',
+          text = ~percent,
+          textposition = 'auto') %>% 
+  layout(title = "Ranks of the 744 Officers in 2016 Allegations",
+         xaxis = list(title = 'Rank of Officers'),
+         yaxis = list(title = 'Number of Complaints against officers'))
+
+
+
+#barplot of rank of officers 2018
+mos_officers_2018 <- allegations_2018 %>%
+  mutate(name = paste(first_name, last_name)) %>% 
+  distinct(name, .keep_all = TRUE) %>% 
+  group_by(rank_incident) %>% 
+  count() %>% 
+  mutate(percent = paste(round(n/744*100, digits = 2),'%', sep = "")) %>% 
+  plot_ly(x = ~rank_incident,
+          y = ~n,
+          type = 'bar',
+          text = ~percent,
+          textposition = 'auto') %>% 
+  layout(title = "Ranks of the 744 Officers in 2018 Allegations",
+         xaxis = list(title = 'Rank of Officers'),
+         yaxis = list(title = 'Number of Complaints against officer'))
+
+
+
+#barplot of types of allegations
+mos_allegations_2016 <- allegations_2016 %>%
+  mutate(name = paste(first_name, last_name)) %>% 
+  distinct(name, .keep_all = TRUE) %>% 
+  group_by(fado_type) %>% 
+  count() %>% 
+  mutate(percent = paste(round(n/744*100, digits = 2),'%', sep = "")) %>% 
+  plot_ly(x = ~fado_type,
+          y = ~n,
+          type = 'bar',
+          text = ~percent,
+          textposition = 'auto') %>% 
+  layout(title = "Types of Allegations in 2016",
+         xaxis = list(title = 'Types of Allegations'),
+         yaxis = list(title = 'Number of Allegations'))
+
+
+
+#barplot of types of allegations
+mos_allegations_2018 <- allegations_2018 %>%
+  mutate(name = paste(first_name, last_name)) %>% 
+  distinct(name, .keep_all = TRUE) %>% 
+  group_by(fado_type) %>% 
+  count() %>% 
+  mutate(percent = paste(round(n/744*100, digits = 2),'%', sep = "")) %>% 
+  plot_ly(x = ~fado_type,
+          y = ~n,
+          type = 'bar',
+          text = ~percent,
+          textposition = 'auto') %>% 
+  layout(title = "Types of Allegations in 2018",
+         xaxis = list(title = 'Types of Allegations'),
+         yaxis = list(title = 'Number of Allegations'))
+
+
+
+#barplot of types of pursued allegations
+mos_pursued_2016 <- allegations_2016 %>%
+  mutate(name = paste(first_name, last_name)) %>% 
+  distinct(name, .keep_all = TRUE) %>% 
+  group_by(board_disposition) %>% 
+  count() %>% 
+  mutate(percent = paste(round(n/744*100, digits = 2),'%', sep = "")) %>% 
+  plot_ly(x = ~board_disposition,
+          y = ~n,
+          type = 'bar',
+          text = ~percent,
+          textposition = 'auto') %>% 
+  layout(title = "Types of Pursued (or Not) Allegations in 2016",
+         xaxis = list(title = 'Types of Allegations'),
+         yaxis = list(title = 'Number of Allegations'))
+
+
+
+#barplot of types of pursued allegations
+mos_pursued_2018 <- allegations_2018 %>%
+  mutate(name = paste(first_name, last_name)) %>% 
+  distinct(name, .keep_all = TRUE) %>% 
+  group_by(board_disposition) %>% 
+  count() %>% 
+  mutate(percent = paste(round(n/744*100, digits = 2),'%', sep = "")) %>% 
+  plot_ly(x = ~board_disposition,
+          y = ~n,
+          type = 'bar',
+          text = ~percent,
+          textposition = 'auto') %>% 
+  layout(title = "Types of Pursued (or Not) Allegations in 2018",
+         xaxis = list(title = 'Types of Allegations'),
+         yaxis = list(title = 'Number of Allegations'))
+
+
+
+
+
+complaintresult <- allegations_2019 %>%
+  plot_ly(x = ~fado_type,
+          y = ~board_disposition,
+          color = ~board_disposition,
+          type = 'bar') %>% 
+  layout(title = "Complaint Results in 2019",
+         xaxis = list(title = "Type of Complaint"))
+
+
+
+
+complaintresult2 <- allegations_2016 %>%
+  group_by(fado_type, board_disposition) %>% 
+  count() %>% 
+  plot_ly(x = ~fado_type,
+          y = ~n,
+          type = 'bar',
+          text = ~board_disposition,
+          hovertemplate = 'Status: %{text}<br>Type of Complaint: %{x}<br>%{y}',
+          color = ~board_disposition,
+          colors = brewer.pal(8, "Paired")
+  ) %>% 
+  layout(title = "Distribution of Complaints by whether they are pursued or not",
+         xaxis = list(title = "Type of Complaints"),
+         yaxis = list(title = "Number of Complaints"),
+         legend = list(title = "Type of Complainant"))
+
+
+
+
+
